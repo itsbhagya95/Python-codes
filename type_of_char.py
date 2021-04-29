485acbc81456cbfc68cc639e51d292f31d360515
unknown <bhagyashripatil2008@gmail.com>
2021-04-29 20:11:50 +0530

    v3

---
+++ type_of_char.py
@@ -0,0 +1,18 @@
+##To check the type of character entered from the keyboard
+
+s=input('Enter any character:')
+if s.isalnum():
+    print('It is alphanumeric character')
+    if s.isalpha():
+        print('It is alphabet symbol')
+        if s.islower():
+            print('It is lowercase alphabet symbol')
+        else:
+            print('It is uppercase alphabet symbol')
+    else:
+        print('It is a digit')
+elif s.isspace():
+    print('It is space character')
+else:
+    print('It is nonspace special character')
+
